1d65b5c53666421a70f118838304bbd375394aa4
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-29 19:45:29 -0400

    imported and installed dependencies

---
+++ employeetracker/employeetracker.js
@@ -0,0 +1,11 @@
+//import dependencies
+const inquirer = require('inquirer');
+const mysql = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'bootcamp',
+    database: 'employee_db'
+});
